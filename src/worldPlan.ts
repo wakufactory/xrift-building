c8bd75503461cf2d5d6a0512324f0e15a8a1465c
waku <waku@lang.co.jp>
2026-05-22 22:04:29 +0900

    open world

--- src/worldPlan.ts
+++ src/worldPlan.ts
@@ -1,5 +1,7 @@
 import type { BuildingPlan } from './building/types'
 
+// north +z east -x south -z west +x
+
 export const worldBuildingPlan: BuildingPlan = {
   unit: 1,
   floorHeight: 5.5,
@@ -19,57 +21,61 @@ export const worldBuildingPlan: BuildingPlan = {
   },
   rooms: [
     {
-      id: 'lobby',
-      position: [0, 0],
-      size: [8, 6],
-      material: {
-        floor: 'floor:stone',
-        wall: 'wall:plaster',
-      },
-      wallColors: {
-        north: '#ded5c5',
-        south: '#dc7310',
+      id: '1-robby',
+      position: [0, 8],
+      size: [8, 10],
+      surfaces: {
+        floor: { materialKey: 'floor:warm-wood' },
+        wall: { materialKey: 'wall:gallery-white' },
+        walls: {
+          north: { color: '#208020' },
+        },
       },
       doors: [
         { side: 'north', offset: 0, width: 2.4, height: 2.25 },
-        { side: 'west', offset: 1.8, width: 2, height: 2.15 },
+        { side: 'east', offset: 2.8, width: 2.1, height: 2.25 },
       ],
       windows: [
-        { side: 'south', offset: -2, width: 1.4, bottom: 1.1, height: 1 },
-        { side: 'south', offset: 2, width: 1.4, bottom: 1.1, height: 1 },
+        { side: 'west', offset: 1, width: 1.5, bottom: 1, height: 1.1 },
+        { side: 'west', offset: -2.5, width: 1.5, bottom: 1, height: 1.1 },
+        { side: 'south', offset: -0, width: 5, bottom: 1.1, height: 2 },
       ],
     },
     {
-      id: 'gallery',
-      position: [0, 8],
-      size: [8, 10],
-      material: {
-        floor: 'floor:warm-wood',
-        wall: 'wall:gallery-white',
+      id: '2-gallery',
+      position: [1, 0],
+      size: [10, 6],
+      surfaces: {
+        ceiling: { hidden: true,noCollider: true },
+        floor: { materialKey: 'floor:stone' },
+        wall: { materialKey: 'wall:plaster' },
+        walls: {
+          south: { color: '#2aee7c' },
+          north: { color: '#dc7310' },
+        },
       },
       doors: [
         { side: 'south', offset: 0, width: 2.4, height: 2.25 },
-        { side: 'east', offset: -2.8, width: 2.1, height: 2.15 },
+        { side: 'west', offset: -1.8, width: 2, height: 2.15 },
       ],
       windows: [
-        { side: 'west', offset: -2., width: 1.5, bottom: 1, height: 1.1 },
-        { side: 'west', offset: 2.5, width: 1.5, bottom: 1, height: 1.1 },
-        { side: 'north', offset: -0, width: 5, bottom: 1.1, height: 2 },
+        { side: 'north', offset: -2, width: 1.4, bottom: 1.1, height: 1 },
+        { side: 'north', offset: 2, width: 1.4, bottom: 1.1, height: 1 },
       ],
     },
     {
       id: 'side-room',
       position: [-5.6, 2.8],
       size: [3.2, 4.6],
-      material: {
-        wall: 'wall:accent',
-      },
-      wallColors: {
-        north: '#607a71',
-        west: '#4c625b',
+      surfaces: {
+        wall: { materialKey: 'wall:accent' },
+        walls: {
+          north: { color: '#607a71' },
+          west: { color: '#4c625b' },
+        },
       },
       doors: [
-        { side: 'east', offset: -0.5, width: 2, height: 2.15 },
+        { side: 'east', offset: -0.5, width: 2, height: 2.25 },
       ],
       windows: [
         { side: 'north', offset: 0, width: 1.4, bottom: 1.05, height: 1 },
